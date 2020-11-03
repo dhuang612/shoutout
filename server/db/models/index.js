3a8c066ef1eca8880715bfabf44562bb11bc7cd8
dhuang612 <dhuang@madisoncollege.edu>
2020-11-03 17:20:38 -0500

    setting up db models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,12 @@
 const User = require('./user')
-
+const Shoutouts = require('./shoutout')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
  *
  *    BlogPost.belongsTo(User)
  */
-
+User.hasMany(Shoutouts)
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -14,5 +14,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Shoutouts
 }
