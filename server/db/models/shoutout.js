90d3e781ecadd1a7ca74a041c1936251a88d5e46
dhuang612 <dhuang@madisoncollege.edu>
2020-11-03 17:20:38 -0500

    setting up db models

---
+++ server/db/models/shoutout.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Shoutout = db.define('shoutout', {
+  user: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    validate: {
+      notEmpty: true
+    }
+  },
+  message: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  from: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Shoutout
