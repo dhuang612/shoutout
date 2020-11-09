7543e3f12509a554a4f5ded431656687df2e669e
dhuang612 <dhuang@madisoncollege.edu>
2020-11-09 17:55:04 -0500

    feat: adding in backend route for adding new shoutouts

---
+++ server/api/shoutouts.js
@@ -0,0 +1,30 @@
+const router = require('express').Router()
+const {User} = require('../db/models')
+
+module.exports = router
+
+//need to post a new one
+router.post('/new', async (req, res, next) => {
+  try {
+    if (req.body) {
+      let from
+      const user = req.user
+      if (!req.body.from) {
+        from = 'N/A'
+      }
+
+      const createNewSO = await user.createShoutout({
+        name: req.body.email,
+        message: req.body.message,
+        from: req.body.from || from
+      })
+      if (createNewSO) {
+        res.status(200).send('successfully made new SO')
+      }
+    }
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+//edit eventually
