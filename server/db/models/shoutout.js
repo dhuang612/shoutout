4fdf63490c9d3e412e75e6327d546ea71b8b5862
dhuang612 <dhuang@madisoncollege.edu>
2020-11-09 16:27:01 -0500

    resolved db association issues

--- server/db/models/shoutout.js
+++ server/db/models/shoutout.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Shoutout = db.define('shoutout', {
-  user: {
+  name: {
     type: Sequelize.STRING,
     validate: {
       notEmpty: true
