f3c85e78008fe120cff59d70b176da5777cd288b
dhuang612 <dhuang@madisoncollege.edu>
2020-11-11 15:20:37 -0500

    feat: added in sendgrid to node js

---
+++ server/emails/mailer.js
@@ -0,0 +1,31 @@
+const sqMail = require('@sendgrid/mail')
+require('../secrets')
+
+sqMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+templates = {
+  welcome: 'd-19822032d2e2456084ebf2dc0a909913',
+  shoutouts: 'd-29f8acda1c92421fadc9d7f578da6e36'
+}
+
+function sendEmail(data) {
+  const msg = {
+    to: data.receiver,
+    from: data.sender,
+    templateId: templates[data.templateName],
+
+    dynamic_template_data: {
+      name: data.name,
+      welcome_url: data.welcome_url
+    }
+  }
+  //send the email
+  sgMail.send(msg, (error, result) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('successfully sent')
+    }
+  })
+}
+exports.sendEmail = sendEmail
