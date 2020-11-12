35ed272810d293aa348b42f47ad3dcba9ca202c5
dhuang612 <dhuang@madisoncollege.edu>
2020-11-12 17:54:40 -0500

    feat: started the route from express for invited users

--- server/auth/index.js
+++ server/auth/index.js
@@ -39,6 +39,30 @@ router.post('/signup', async (req, res, next) => {
     }
   }
 })
+//route to handle invited people // will need to send the id along from the invite side.
+router.post('/signup/invite', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    if (req.body.inviteId) {
+      user.flag = false
+      await user.save()
+    }
+    if (user.usedValidEmail(req.body.email)) {
+      //give them access to the user who invite them's email list.
+      req.login(user, err => (err ? next(err) : res.json(user)))
+    } else {
+      const removeUser = await User.findByPk(user.id)
+      await removeUser.destroy(req.body)
+      res.status(401).send('Invalid email!')
+    }
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists')
+    } else {
+      next(err)
+    }
+  }
+})
 
 router.post('/logout', (req, res) => {
   req.logout()
