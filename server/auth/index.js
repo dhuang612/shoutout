7ad764ff4a8f343fec9eb5544c6885fa09e036e6
dhuang612 <dhuang@madisoncollege.edu>
2020-11-17 15:33:45 -0500

    feat: can now signup as an invited user

--- server/auth/index.js
+++ server/auth/index.js
@@ -42,21 +42,23 @@ router.post('/signup', async (req, res, next) => {
 //route to handle invited people // will need to send the id along from the invite side.
 router.post('/signup/invite', async (req, res, next) => {
   try {
-    console.log('we are hitting this route!')
-    const user = await User.create(req.body)
-
     if (req.body.id) {
+      console.log(req.body)
+      const password = req.body.password
+      const email = req.body.email
+      const user = await User.create({email, password})
       user.flag = false
       user.inviteId = req.body.id
       await user.save()
-    }
-    if (user.usedValidEmail(req.body.email)) {
-      //give them access to the user who invite them's email list.
-      req.login(user, err => (err ? next(err) : res.json(user)))
-    } else {
-      const removeUser = await User.findByPk(user.id)
-      await removeUser.destroy(req.body)
-      res.status(401).send('Invalid email!')
+
+      if (user.usedValidEmail(req.body.email)) {
+        //give them access to the user who invite them's email list.
+        req.login(user, err => (err ? next(err) : res.json(user)))
+      } else {
+        const removeUser = await User.findByPk(user.id)
+        await removeUser.destroy(req.body)
+        res.status(401).send('Invalid email!')
+      }
     }
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
